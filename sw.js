2848605e62c38545de54bc3815ae5e743aeb8384
kosalanuwan <kosalanuwan@users.noreply.github.com>
2021-01-31 08:05:08 +0000

    deploy: kosalanuwan/gh-site-journal@bbda41e6fc96cd22b0027feb72d1ece55071d77a

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/journal/assets/css/main.css?202101220713",
+        "/journal/assets/css/main.css?202101310805",
         "/journal/"
       ]);
     })
